5d76e61490cb0bd4850b6c2e17070d5484781244
zjjcmnv <kaick.09@gmail.com>
2021-02-04 15:29:57 -0300

    adicionando imagem

--- index.php
+++ index.php
@@ -11,13 +11,25 @@ h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 <h7>QUERO DELETAR TUDO </h7>
 
 <div style = "display:flex">
-<div style = "width:200px;height:200px;background:yellow;padding:50px;margin:10px">
+<div style = "width:200px;height:400px;background:yellow;padding:50px;margin:10px">
     Um teste em um quadrado teste
     <img src="b2.jpg" style="width:200px">
+    <br>
+    R$ 850,00
+    <br>
+    <a href ="" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">comprar agora </a>
+    <a href ="" target="_blank" style="color:white">Enrar em contato com o vendedor</a>
+
 </div>
-<div style = "width:200px;height:200px;background:blue;padding:50px;margin:10px">
+
+<div style = "width:200px;height:400px;background:blue;padding:50px;margin:10px">
     Um teste em um quadrado teste
     <img src="b1.jpg" style="width:200px">
+    <br>
+    R$ 550,00
+    <a href ="" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">comprar agora </a>
+    <a href ="" target="_blank" style="color:white">Enrar em contato com o vendedor</a>
+
 </div>
 
 </div>
