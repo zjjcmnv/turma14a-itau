c009efafe0f4aeb7f02143ff18fb522999eedd4a
zjjcmnv <kaick.09@gmail.com>
2021-02-04 14:56:46 -0300

    alteracao

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
+h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
 <h3> Um deploy aotomático feito com alunos do itaú </h3>
 
@@ -10,6 +10,12 @@
 <h10>Tentar sempre, desistir as vezes!!!<h10>
 <h7>QUERO DELETAR TUDO </h7>
 
-<div style = "width:200px;height:200px;background:yellow">
+<div style = "display:flex">
+<div style = "width:200px;height:200px;background:yellow;padding:50px;margin:5px">
     Um teste em um quadrado teste
+</div>
+<div style = "width:200px;height:200px;background:blue;padding:50p;margin:5px">
+    Um teste em um quadrado teste
+</div>
+
 </div>
